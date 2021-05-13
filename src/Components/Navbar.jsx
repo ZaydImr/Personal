7214a859a60ba4cf22351c12a36dc9ef9ef9d355
Zayd Elimrani <plutorider47@gmail.com>
2021-05-13 22:34:15 +0000

    Creating the navbar component and editing the app component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+      return (
+            <nav>
+                  <a className="logo">Zayd</a>
+                  <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Experience</li>
+                  </ul>
+            </nav>
+      )
+}
+
+export default Navbar
