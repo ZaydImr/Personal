028b5da6953e4c5e19847b3cdb881c3a8117bb55
Zayd EL IMRANI <elimrani.z@gmail.com>
2022-09-18 21:50:15 +0100

    Adding link to my resume

--- src/components/NotFinished.jsx
+++ src/components/NotFinished.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const NotFinished = () => {
     return (
         <div id='NotFinished' className="not-finished container" style={{width: '80%', margin: 'auto'}}>
-            <div style={{ margin:'auto', marginBottom: '2rem' }}>
+            <div>
                 The website isn't finished yet .
                 <a href="https://github.com/ZaydImr/Personal" target="_blank" rel="noopener noreferrer">Check the repository .</a>
+            </div>
+            <div style={{ marginBottom: '2rem', marginTop: "1rem" }}>
+                <a href="https://drive.google.com/file/d/1mx__wFU1W4s878aCpo3ORNL2OFAHCg3r/view?usp=sharing" target="_blank" rel="noopener noreferrer">or you can check my resume (CV) here</a>
             </div>
         </div>
     )
