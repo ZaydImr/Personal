eaadfa726ac14b20c87b67b8cc478227ef372074
Zayd EL IMRANI <elimrani.z@gmail.com>
2022-09-22 13:53:55 +0100

    Enabling analytics

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+getAnalytics(app);
 export const db = getFirestore(app);
 
 export default app;
